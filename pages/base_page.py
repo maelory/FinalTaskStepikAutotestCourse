ac981678fb4a330e0b2afea80a2a679bee246956
Aleksej Multan <aleksejmultan@outlook.com>
2022-01-23 16:36:00 +0200

    login page links locators are moved to BasePageLocators, LoginLink tests are added to test_product_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,7 @@ import math
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, NoAlertPresentException
 from selenium.webdriver.support.wait import WebDriverWait
+from .locators import BasePageLocators
 
 
 class BasePage:
@@ -12,6 +13,13 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
 
     def is_element_present(self, how, what):
         try:
--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ import time
 
 import pytest
 
+from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 
 base_url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -49,3 +50,18 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     time.sleep(1)
     page.add_to_cart_message_is_disappeared()
 
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
